f18ac93c9200abbd328b719128b7e189b7d0dbe6
Willian Wallace <30538522+willianIF@users.noreply.github.com>
2019-12-16 12:01:20 -0300

    Configuração do ambiente
    Configuração inicial do ambiente de desenvolvimento

---
+++ src/pages/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Routes from '../routes.js';
+
+import '../css/app.css';
+
+class App extends Component {
+
+  render() {
+    return (
+      <h1>TESTE</h1>
+      // <Routes />
+    );
+  }
+}
+
+export default App;
