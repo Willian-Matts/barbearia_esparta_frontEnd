2c0973831e7ece45a99bca76639c1cc510816841
Willian Wallace <30538522+willianIF@users.noreply.github.com>
2020-02-07 16:02:26 -0200

    Configuração do css dos card

--- src/components/CardServico.js
+++ src/components/CardServico.js
@@ -10,10 +10,10 @@ export default function CardServico(props) {
                 <img src={props.img} alt="Foto do serviço" />
                 <p>{props.cardinfo}</p>
             </Container> */}
-            <Card style={{ width: '18rem' }}>
+            <Card style={{ width: '18rem' }} id="card">
                 <Card.Img variant="top" src={props.img} />
-                <Card.Body>
-                    <Card.Title>{props.cardTitulo}</Card.Title>
+                <Card.Body className="box-card">
+                    <Card.Title><h1>{props.cardTitulo}</h1></Card.Title>
                     <Card.Text>
                         Some quick example text to build on the card title and make up the bulk of
                         the card's content.
