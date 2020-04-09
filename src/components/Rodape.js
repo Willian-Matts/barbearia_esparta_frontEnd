c5bcda74bf2c390865a369f7f76c51a9d204c619
gusttavogomes <30482020+gusttavogomes@users.noreply.github.com>
2020-04-09 19:19:49 -0300

    Update Rodape.js

--- src/components/Rodape.js
+++ src/components/Rodape.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 
 import '../css/Rodape.css';
 import logoVetor from '../image/logo/Logo-3.svg';
-import instagran from '../image/logo/Logo-3.svg';
-import { Jumbotron, Container, Nav } from 'react-bootstrap'
-import Glowingbutton from './GlowingButton';
+//import instagran from '../image/logo/Logo-3.svg';
+import { Jumbotron, Container, Nav, Button } from 'react-bootstrap'
+//import Glowingbutton from './GlowingButton';
 
 
 export default class Rodape extends Component {
@@ -14,12 +14,14 @@ export default class Rodape extends Component {
                 <Jumbotron id="box-rodape">
                     <Container id="box-redes-sociais">
                         {/* redes sociais  */}
-                        <Glowingbutton value={instagran} alt="teste"/>
+                        {/*<Glowingbutton value={instagran} alt="teste"/>*/}
+                        <Button href="https://www.instagram.com/ronaldinho">                            
+                        </Button>
                     </Container>
                     <Container id="box-Logo-rodape">
                         <Nav.Link href="#box-carrosel">
                             <img src={logoVetor} alt="Logo Barbearia" />
-                        </Nav.Link>
+                        </Nav.Link>                        
                     </Container>
                 </Jumbotron>
             </>
